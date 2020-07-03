5c0a3b5df26f47516f817dc89b5c13e18488f64d
KazuyaSasaki <kazuya.sasaki39@gmail.com>
2020-07-04 01:26:28 +0900

    Tymeleafの使い方

--- src/main/java/com/sample/controller/IndexController.java
+++ src/main/java/com/sample/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.sample.controller;
 
+import java.util.ArrayList;
+import java.util.Date;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,9 +15,38 @@ public class IndexController {
    @RequestMapping(value = "/", method = RequestMethod.GET)
    public String index(Model model) {
 
-    model.addAttribute("message", "This is sample page");   
+       ArrayList<Article> articles = new ArrayList<Article>();
+       articles.add(new Article("Tom", "Hello World!!"));
+       articles.add(new Article("Alex", "Let's enjoy Springbooooooot!!", false));
+       articles.add(new Article("Emily", "We can do it!"));
+       articles.add(new Article("Ann",  "Awesome!!"));
+
+       model.addAttribute("message", "This is sample page");    
+       model.addAttribute("articles", articles); 
 
        return "index";
+   }
+
+
+   class Article {
+
+       public String postName;
+       public Date postDate;
+       public String postText;
+       public boolean isPublished;
+
+       Article(String name, String text) {
+           this.postName = name;
+           this.postDate = new Date();
+           this.postText = text;
+           this.isPublished = true;
+       }
+
+       Article(String name, String text, boolean isPublished) {
+           this(name, text);
+           this.isPublished = isPublished;
+       }
+
    }
 
 }
